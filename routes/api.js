048ac480ac85ff352e6520a0aa0035b00683f5ee
Brian Yang <brian@brianyang.me>
2015-09-13 14:19:59 -0400

    Implement required POST and GET APIs

---
+++ routes/api.js
@@ -0,0 +1,82 @@
+/**
+ * Wiki API calls
+ */
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cloudwiki');
+
+var Article = mongoose.model('Article', {
+    title: String,
+    body: String
+});
+
+/**
+ * Get article by name
+ */
+router.get('/:title', function(req, res, next) {
+
+    Article.find({
+        title: req.params.title.replace(/_/g, " ")
+    }).lean().exec(function(err, article) {
+        res.setHeader('content-type', 'application/json');
+
+        // Mongo returns an array, but we only want one
+        return res.send(JSON.stringify(article[0]));
+    });
+});
+
+/**
+ * Create or update article by name
+ */
+router.post('/:title', function(req, res, next) {
+
+    Article.update({
+        title: req.params.title.replace(/_/g, " ")
+    }, {
+        title: req.body.title || req.params.title.replace(/_/g, " "),
+        body: req.body.body
+
+    }, function(err, number, raw) {
+
+        var status = {
+            status: 'null'
+        };
+
+        res.setHeader('content-type', 'application/json');
+
+        if (err) {
+            console.log("\nError updating article");
+            status.status = 'ERROR-UPDATE';
+        } else {
+            console.log("\nUpdated article - " + req.params.title);
+            status.status = 'UPDATED';
+        }
+
+        if (!err && !number.n) {
+
+            var article = new Article({
+                title: req.params.title.replace(/_/g, " "),
+                body: req.body.body
+            });
+
+            article.save(function(err) {
+                if (err) {
+                    console.log("\nError creating new article");
+                    status.status = 'ERROR-CREATE';
+                } else {
+                    console.log("\nNew article - " + req.params.title + " created");
+                    status.status = 'CREATED';
+                }
+                return res.send(JSON.stringify(status));
+            });
+        } else {
+            return res.send(JSON.stringify(status));
+        }
+
+    });
+
+});
+
+module.exports = router;
