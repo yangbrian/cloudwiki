03c5d7fb4765755e4ebe9ff0eb754c33c197b7a4
Xinwen Wang <wxwjason@gmail.com>
2015-10-21 15:09:17 -0400

    add more db routers and change update title

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://10.0.0.12:27017,10.0.0.13:27017,10.0.0.14:27017/cloudwiki',{ mongos : true}, function(err) {
+mongoose.connect('mongodb://10.0.0.12:27017,10.0.0.13:27017,10.0.0.14:27017,10.0.0.36:27017,10.0.0.38:27017,10.0.0.45:27017/cloudwiki',{ mongos : true}, function(err) {
   if (!err)
     console.log(err);
 });
